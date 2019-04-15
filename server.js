e2aa12f40ce798bd28f8750a2d6e2c61b36034dd
HaifaSeyam <h.abuamro@hotmail.com>
2019-04-14 19:17:42 -0500

    nothing

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 require("./routes/apiRoutes")(app);
